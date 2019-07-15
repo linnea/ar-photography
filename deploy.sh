07919a84f6db40d023cb4c5cbcbd4521a952f8b5
linnea.watson <watson@squareup.com>
2019-07-15 13:46:45 -0700

    files

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:linneakw/ar-photo.git master:gh-pages
+git push -f git@github.com:linneakw/ar-photography.git master:gh-pages
 
 cd -
