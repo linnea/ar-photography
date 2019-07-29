18b8fc7ceb9fd0e63953d7e15dbf2a84ac8d9a89
linnea.watson <watson@squareup.com>
2019-07-29 13:55:23 -0700

    files

---
+++ pages/astronaut.vue
@@ -0,0 +1,16 @@
+<template>
+	<model-viewer
+		class="viewer"
+		src="/assets/astrobaut.glb"
+		auto-rotate
+		camera-controls
+		ar>
+	</model-viewer>
+</template>
+
+<script>
+import '@google/model-viewer';
+export default {
+	name: 'Astronaut',
+};
+</script>
---
+++ pages/goggles.vue
@@ -0,0 +1,16 @@
+<template>
+	<model-viewer
+		class="viewer"
+		src="/assets/goggles2.glb"
+		auto-rotate
+		camera-controls
+		ar>
+	</model-viewer>
+</template>
+
+<script>
+import '@google/model-viewer';
+export default {
+	name: 'Goggles',
+};
+</script>
